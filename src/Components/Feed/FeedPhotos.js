b33f86db2991129011fb2fa587c573309f214205
Ronaldo Reis <ronaldocreis7@gmail.com>
2022-09-27 12:56:59 -0300

    feed

---
+++ src/Components/Feed/FeedPhotos.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { PHOTOS_GET } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../../Interface/Error';
+import Loading from '../../Interface/Loading';
+import FeedPhotosItem from './FeedPhotosItem';
+import styles from './FeedPhotos.module.css';
+
+const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      await request(url, options);
+    }
+    fetchPhotos();
+  }, [request]);
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  else return null;
+};
+
+export default FeedPhotos;
